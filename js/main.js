51ca6642cc397972c342e5470c9857141d217f37
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-10 20:06:49 +0530

    data fetch complite

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const lodeAllRecipes = async () => {
+      const res = await fetch("https://dummyjson.com/recipes")
+      const data = await res.json()
+      displayRecipes(data.recipes)
+
+}
+
+lodeAllRecipes()
+
+
+const displayRecipes = (data) => {
+      const recipeCont = document.getElementById("recipesCont")
+      data.map(recipe => {
+            const { name, tags } = recipe
+            console.log(tags);
+
+            let spns = ""
+
+            tags.forEach(tag => {
+                  spns += `<div class="badge badge-outline">${tag}</div>`
+            })
+            const recipeCard = document.createElement("div")
+            recipeCard.innerHTML = `
+             <h1>${name}</h1>
+             <div class="flex gap-3"> ${spns} </div>
+            
+            `
+            recipeCont.appendChild(recipeCard)
+
+
+      })
+}
