50af930c1a33c7130ad7674cf81b368b87433432
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-10 20:42:15 +0530

    btn color added

--- js/main.js
+++ js/main.js
@@ -17,15 +17,15 @@ const displayRecipes = (data) => {
             let spns = ""
 
             tags.forEach(tag => {
-                  spns += `<div class="badge badge-outline ">${tag}</div>`
+                  spns += `<div class="badge badge-outline border-red-300 ">${tag}</div>`
             })
             const recipeCard = document.createElement("div")
-            recipeCard.classList = " border p-5 rounded-lg shadow-md"
+            recipeCard.classList = " border p-5 rounded-lg shadow-md flex flex-col gap-3 justify-between"
             recipeCard.innerHTML = `
              <img  class=" rounded-lg w-full " src="${image}">
              <h1 class=" text-2xl font-bold my-5">${name}</h1>
              <div class="flex gap-3 flex-wrap"> ${spns} </div>
-             <button class="btn mt-6">Cook this</button>
+             <button class="btn mt-6 w-full bg-red-400">Cook this</button>
             
             `
             recipeCont.appendChild(recipeCard)
