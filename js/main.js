b43de21055613ccac2c060a9e87977df24a40d43
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-10 20:30:57 +0530

    card complite

--- js/main.js
+++ js/main.js
@@ -17,13 +17,15 @@ const displayRecipes = (data) => {
             let spns = ""
 
             tags.forEach(tag => {
-                  spns += `<div class="badge badge-outline">${tag}</div>`
+                  spns += `<div class="badge badge-outline ">${tag}</div>`
             })
             const recipeCard = document.createElement("div")
+            recipeCard.classList = " border p-5 rounded-lg shadow-md"
             recipeCard.innerHTML = `
-             <img src="${image}">
-             <h1>${name}</h1>
-             <div class="flex gap-3"> ${spns} </div>
+             <img  class=" rounded-lg w-full " src="${image}">
+             <h1 class=" text-2xl font-bold my-5">${name}</h1>
+             <div class="flex gap-3 flex-wrap"> ${spns} </div>
+             <button class="btn mt-6">Cook this</button>
             
             `
             recipeCont.appendChild(recipeCard)
